fe98c4823f4d25ec3a0b41dd958f3d998a2a0abb
Hai.HoangDuc <hai.hoangduc@skybull.studio>
2025-04-16 17:45:06 +0700

    update loader

--- Build/meme-app.loader.js
+++ Build/meme-app.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 78682002,
+    wasmFileSize: 78681787,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
