0087b081ac1c1fe0bdcc5a1f93ecee2c3e0b633c
Hai.HoangDuc <hai.hoangduc@skybull.studio>
2025-04-24 10:00:34 +0700

    update meme data

--- Build/meme-app.framework.js
+++ Build/meme-app.framework.js
@@ -1288,10 +1288,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  6211968: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 6212023: ($0) => { performance.now = function() { return $0; }; },  
+  6212016: () => { Module['emscripten_get_now_backup'] = performance.now; },  
  6212071: ($0) => { performance.now = function() { return $0; }; },  
- 6212119: () => { performance.now = Module['emscripten_get_now_backup']; }
+ 6212119: ($0) => { performance.now = function() { return $0; }; },  
+ 6212167: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/meme-app.loader.js
+++ Build/meme-app.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 78830671,
+    wasmFileSize: 78831590,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
